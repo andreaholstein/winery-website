dc70525c73d7d0b1db79b71bbb4658bc149dde0e
Andrea H <andrea.gitcommit@gmail.com>
2025-02-27 13:46:47 -0500

    Update Typo, import fonts, fix width and padding

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Winery Website
 
-This web application is a website for a mock winery, showcasing visual design, navigation and contact functionality, and a chance for me to show off my knowledge of React and front-end development.
+This React.js web application is a website for a mock winery, showcasing visual design, navigation and contact functionality.
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,25 @@ import "./Footer.scss";
 
 function Footer() {
   return (
-    <>
-    </>
+    <section className="footer">
+      <div className="footer__wrap">
+        <ul className="footer__list">
+          <li className="footer__list-item"><h4 className="footer__list-header"></h4></li>
+          <li className="footer__list-item"><a href="" className="footer__list-link"><p className="footer__list-text"></p></a></li>
+          <li className="footer__list-item"><a href="" className="footer__list-link"><p className="footer__list-text"></p></a></li>
+        </ul>
+        <ul className="footer__list">
+          <li className="footer__list-item"><h4 className="footer__list-header"></h4></li>
+          <li className="footer__list-item"><a href="" className="footer__list-link"><p className="footer__list-text"></p></a></li>
+          <li className="footer__list-item"><a href="" className="footer__list-link"><p className="footer__list-text"></p></a></li>
+        </ul>
+        <ul className="footer__list">
+          <li className="footer__list-item"><h4 className="footer__list-header"></h4></li>
+          <li className="footer__list-item"><a href="" className="footer__list-link"><p className="footer__list-text"></p></a></li>
+          <li className="footer__list-item"><a href="" className="footer__list-link"><p className="footer__list-text"></p></a></li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
